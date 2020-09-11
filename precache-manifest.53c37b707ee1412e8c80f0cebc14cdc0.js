eda63ad9d4501970c3ea078cd7c8ad0c27b04508
kckc4u <vinodvaishnav@MacBook-Pro.local>
2020-09-11 11:40:40 +0530

    Updates

---
+++ precache-manifest.53c37b707ee1412e8c80f0cebc14cdc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd4692b8048c588f0b881db9c27fae0",
+    "url": "/react-search-monster/index.html"
+  },
+  {
+    "revision": "5d2d09654cce67414797",
+    "url": "/react-search-monster/static/css/main.49b5615f.chunk.css"
+  },
+  {
+    "revision": "568889cdc8aaae04858f",
+    "url": "/react-search-monster/static/js/2.f9a3e1c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-search-monster/static/js/2.f9a3e1c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2d09654cce67414797",
+    "url": "/react-search-monster/static/js/main.8e6dcaee.chunk.js"
+  },
+  {
+    "revision": "690cbce9f1e4cd7b9100",
+    "url": "/react-search-monster/static/js/runtime-main.631c0a98.js"
+  }
+]);
